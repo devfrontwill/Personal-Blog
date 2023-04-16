76c8a6aca25da34230e08e11cbd8ec69f34e7c96
Wiliam Roza <devfrontwill@gmail.com>
2023-04-16 12:42:45 -0300

    criado e estilizado o componente banner do header

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./componentes/Menu";
 import Inicio from './paginas/Inicio';
 import SobreMim from './paginas/SobreMim';
 
-function App() {
+function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
+      <Menu />
 
+      <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/sobremim" element={<SobreMim />} />
         <Route path="*" element={<div>Página não encontrada</div>} />
-        
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
+export default AppRoutes;
