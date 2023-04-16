a189d544698160ff05a20af28c3759c7ff1baad0
Wiliam Roza <devfrontwill@gmail.com>
2023-04-16 10:36:40 -0300

    instalando e criando as primeiras rotas com react-router-dom6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-function App () {
-  return (
-    
-    <h1>Personal Blod</h1>
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from './paginas/Inicio';
+import SobreMim from './paginas/SobreMim';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
